30d431832d95295ec0f0fdb3c99baa3ee66becff
Jane Kostevich <yana.kostevich18@gmail.com>
2023-03-07 00:58:56 +0200

    routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const {routes} = require('./src/routes');
 require('dotenv').config()
 
 const PORT = process.env.PORT;
@@ -16,6 +17,10 @@ mongoose
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    routes.forEach(item => {
+      app.use(item, require(`./src/routes/${item}`));
+    });
 
     app.listen(PORT, (error) => {
       error ? console.log(error) : console.log(`listening port ${PORT}`);
---
+++ src/controllers/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    category: require ('./category.controller'),
+    users: require ('./users.controller'), 
+}
