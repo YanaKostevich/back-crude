529801b099c52b71102d494e8a6f5f27eac76cf1
Jane Kostevich <yana.kostevich18@gmail.com>
2023-03-08 00:24:15 +0200

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const { routes } = require('./src/routes')
 require('dotenv').config()
 
@@ -15,6 +16,7 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
